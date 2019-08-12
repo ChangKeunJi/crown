00d387b65b3f9eb93922415468ba76aa5669fdaa
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-08-12 14:58:49 +1000

    adding header component and links to home, shop page and contack

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      {/* <Route path="/hats/:id" component={HatsPage} /> */}
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
