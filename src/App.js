c3a0470b9ce33aa4048a6408bb4cab1f522c1690
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-08-12 14:29:46 +1000

    Updating app with a shop page, collection preview component and collection item component.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = props => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push("/")}>Go Back</button>
-      <h1>Hats Page: {props.match.params.id}</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
       {/* <Route path="/hats/:id" component={HatsPage} /> */}
     </div>
   );
